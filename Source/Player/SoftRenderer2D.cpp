16fcc9c61c3b1e0d5598cab338878ac42141e0fa
ideugu <guru@dustinlee.me>
2021-01-22 16:56:13 +0900

    최종 예제

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -68,11 +68,20 @@ void SoftRenderer::Update2D(float InDeltaSeconds)
 	static float scaleMin = 10.f;
 	static float scaleMax = 20.f;
 	static float scaleSpeed = 20.f;
+	static float duration = 1.5f;
+	static float elapsedTime = 0.f;
+
+	// 경과 시간과 사인함수를 활용한 [0,1]값의 생성
+	elapsedTime += InDeltaSeconds;
+	elapsedTime = Math::FMod(elapsedTime, duration);
+	float currentRad = (elapsedTime / duration) * Math::TwoPI;
+	float alpha = (sinf(currentRad) + 1) * 0.5f;
 
 	Vector2 inputVector = Vector2(input.GetAxis(InputAxis::XAxis), input.GetAxis(InputAxis::YAxis)).GetNormalize();
 	deltaPosition = inputVector * moveSpeed * InDeltaSeconds;
-	float deltaScale = input.GetAxis(InputAxis::ZAxis) * scaleSpeed * InDeltaSeconds;
-	currentScale = Math::Clamp(currentScale + deltaScale, scaleMin, scaleMax);
+
+	// [0,1]을 활용해 현재 스케일 값을 계산
+	currentScale = Math::Lerp(scaleMin, scaleMax, alpha);
 }
 
 // 렌더링 로직을 담당하는 함수
