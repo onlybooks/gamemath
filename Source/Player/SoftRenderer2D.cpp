06113550cf4fc77edd21ad632721f69ce7cdd6d9
ideugu <guru@dustinlee.me>
2021-01-22 20:32:48 +0900

    최종 예제

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -55,6 +55,7 @@ void SoftRenderer::LoadScene2D()
 // 게임 로직과 렌더링 로직이 공유하는 변수
 float currentScale = 10.f;
 float deltaDegree = 0.f;
+float deltaShear = 0.f;
 
 // 게임 로직을 담당하는 함수
 void SoftRenderer::Update2D(float InDeltaSeconds)
@@ -68,7 +69,13 @@ void SoftRenderer::Update2D(float InDeltaSeconds)
 	static float scaleMax = 10.f;
 	static float scaleSpeed = 10.f;
 	static float rotateSpeed = 180.f;
+	static float shearSpeed = 2.f;
 
+	deltaShear = input.GetAxis(InputAxis::XAxis) * shearSpeed * InDeltaSeconds;
+	float deltaScale = input.GetAxis(InputAxis::ZAxis) * scaleSpeed * InDeltaSeconds;
+	deltaDegree = input.GetAxis(InputAxis::WAxis) * rotateSpeed * InDeltaSeconds;
+
+	currentScale = Math::Clamp(currentScale + deltaScale, scaleMin, scaleMax);
 }
 
 // 렌더링 로직을 담당하는 함수
@@ -83,8 +90,11 @@ void SoftRenderer::Render2D()
 
 	// 렌더링 로직의 로컬 변수
 	static float currentDegree = 0.f;
+	static float currentShear = 0.f;
+	static const Vector2 pivot(200.f, 0.f);
 
 	currentDegree += deltaDegree;
+	currentShear += deltaShear;
 
 	// 하트를 구성하는 점 생성
 	static float increment = 0.001f;
@@ -105,6 +115,64 @@ void SoftRenderer::Render2D()
 		}
 	}
 
+	// 크기 행렬
+	Vector2 sBasis1(currentScale, 0.f);
+	Vector2 sBasis2(0.f, currentScale);
+	Matrix2x2 sMatrix(sBasis1, sBasis2);
+
+	// 회전 행렬
+	float sin, cos;
+	Math::GetSinCos(sin, cos, currentDegree);
+	Vector2 rBasis1(cos, sin);
+	Vector2 rBasis2(-sin, cos);
+	Matrix2x2 rMatrix(rBasis1, rBasis2);
+
+	// 밀기 행렬
+	Vector2 shBasis1 = Vector2::UnitX;
+	Vector2 shBasis2(currentShear, 1.f);
+	Matrix2x2 shMatrix(shBasis1, shBasis2);
+
+	// 합성 행렬
+	Matrix2x2 cMatrix = shMatrix * rMatrix * sMatrix;
+
+	// 크기 행렬의 역행렬
+	float invScale = 1.f / currentScale;
+	Vector2 isBasis1(invScale, 0.f);
+	Vector2 isBasis2(0.f, invScale);
+	Matrix2x2 isMatrix(isBasis1, isBasis2);
+
+	// 회전 행렬의 역행렬
+	Matrix2x2 irMatrix = rMatrix.Tranpose();
+
+	// 밀기 행렬의 역행렬
+	Vector2 ishBasis1 = Vector2::UnitX;
+	Vector2 ishBasis2(-currentShear, 1.f);
+	Matrix2x2 ishMatrix(ishBasis1, ishBasis2);
+
+	// 역행렬의 합성행렬. ( 역순으로 조합하기 )
+	Matrix2x2 icMatrix = isMatrix * irMatrix * ishMatrix;
+
+	// 각 값을 초기화한 후 동일하게 증가시키면서 색상 값을 지정
+	rad = 0.f;
+	HSVColor hsv(0.f, 1.f, 0.85f);
+	for (auto const& v : hearts)
+	{
+		// 왼쪽 하트 ( 변환 행렬을 적용하기 )
+		Vector2 left = cMatrix * v;
+		r.DrawPoint(left - pivot, hsv.ToLinearColor());
+
+		// 오른쪽 하트 ( 왼쪽 하트에 추가로 역행렬을 적용하기 )
+		Vector2 right = icMatrix * left;
+		r.DrawPoint(right + pivot, hsv.ToLinearColor());
+
+		hsv.H = rad / Math::TwoPI;
+		rad += increment;
+	}
+
+	// 현재 밀기, 스케일, 회전각을 화면에 출력
+	r.PushStatisticText(std::string("Shear : ") + std::to_string(currentShear));
+	r.PushStatisticText(std::string("Scale : ") + std::to_string(currentScale));
+	r.PushStatisticText(std::string("Degree : ") + std::to_string(currentDegree));
 }
 
 // 메시를 그리는 함수
