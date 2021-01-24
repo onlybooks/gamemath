a42b3699703e887e6eb8c6b19430249de15c2cfc
ideugu <guru@dustinlee.me>
2021-01-24 18:26:42 +0900

    실습예제 템플릿

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -53,7 +53,10 @@ void SoftRenderer::LoadScene2D()
 }
 
 // 게임 로직과 렌더링 로직이 공유하는 변수
-
+Vector2 playerPosition(0.f, 0.f);
+LinearColor playerColor = LinearColor::Gray;
+Vector2 targetPosition(100.f, 100.f);
+LinearColor targetColor = LinearColor::Blue;
 
 // 게임 로직을 담당하는 함수
 void SoftRenderer::Update2D(float InDeltaSeconds)
@@ -63,7 +66,43 @@ void SoftRenderer::Update2D(float InDeltaSeconds)
 	const InputManager& input = g.GetInputManager();
 
 	// 게임 로직의 로컬 변수
+	static float moveSpeed = 100.f;
+	static std::random_device rd;
+	static std::mt19937 mt(rd());
+	static std::uniform_real_distribution<float> randomPosX(-300.f, 300.f);
+	static std::uniform_real_distribution<float> randomPosY(-200.f, 200.f);
+	static float duration = 3.f;
+	static float elapsedTime = 0.f;
+	static Vector2 targetStart = targetPosition;
+	static Vector2 targetDestination = Vector2(randomPosX(mt), randomPosY(mt));
 
+	elapsedTime = Math::Clamp(elapsedTime + InDeltaSeconds, 0.f, duration);
+
+	// 지정한 시간이 경과하면 새로운 이동 지점을 랜덤하게 설정
+	if (elapsedTime == duration)
+	{
+		targetStart = targetDestination;
+		targetPosition = targetDestination;
+		targetDestination = Vector2(randomPosX(mt), randomPosY(mt));
+
+		elapsedTime = 0.f;
+	}
+	else // 비율에 따라 목표지점까지 선형보간하면서 이동
+	{
+		float ratio = elapsedTime / duration;
+		targetPosition = Vector2(
+			Math::Lerp(targetStart.X, targetDestination.X, ratio),
+			Math::Lerp(targetStart.Y, targetDestination.Y, ratio)
+		);
+	}
+
+	Vector2 inputVector = Vector2(input.GetAxis(InputAxis::XAxis), input.GetAxis(InputAxis::YAxis)).GetNormalize();
+	Vector2 deltaPosition = inputVector * moveSpeed * InDeltaSeconds;
+
+	// 물체의 최종 상태 설정
+	playerColor = LinearColor::Gray;
+	targetColor = LinearColor::Blue;
+	playerPosition += deltaPosition;
 }
 
 // 렌더링 로직을 담당하는 함수
@@ -73,11 +112,43 @@ void SoftRenderer::Render2D()
 	auto& r = GetRenderer();
 	const auto& g = Get2DGameEngine();
 
-	// 배경에 격자 그리기
-	DrawGizmo2D();
-
 	// 렌더링 로직의 로컬 변수
+	static float radius = 5.f;
+	static std::vector<Vector2> sphere;
+	static float sightLength = 300.f;
 
+	if (sphere.empty())
+	{
+		for (float x = -radius; x <= radius; ++x)
+		{
+			for (float y = -radius; y <= radius; ++y)
+			{
+				Vector2 target(x, y);
+				float sizeSquared = target.SizeSquared();
+				float rr = radius * radius;
+				if (sizeSquared < rr)
+				{
+					sphere.push_back(target);
+				}
+			}
+		}
+	}
+
+	// 플레이어 렌더링. 
+	for (auto const& v : sphere)
+	{
+		r.DrawPoint(v + playerPosition, playerColor);
+	}
+
+	// 타겟 렌더링
+	for (auto const& v : sphere)
+	{
+		r.DrawPoint(v + targetPosition, targetColor);
+	}
+
+	// 주요 정보 출력
+	r.PushStatisticText(std::string("Player Position : ") + playerPosition.ToString());
+	r.PushStatisticText(std::string("Target Position : ") + targetPosition.ToString());
 }
 
 // 메시를 그리는 함수
