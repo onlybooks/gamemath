bc06236afd8b5759777c18a631ccc0f363c780f4
ideugu <guru@dustinlee.me>
2021-01-27 12:27:37 +0900

    최종 예제

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -60,54 +60,92 @@ Vector2 lineEnd(400.f, 0.f);
 // 게임 로직을 담당하는 함수
 void SoftRenderer::Update2D(float InDeltaSeconds)
 {
-	// 게임 로직에서 사용하는 모듈 내 주요 레퍼런스
-	auto& g = Get2DGameEngine();
-	const InputManager& input = g.GetInputManager();
+    // 게임 로직에서 사용하는 모듈 내 주요 레퍼런스
+    auto& g = Get2DGameEngine();
+    const InputManager& input = g.GetInputManager();
 
-	// 게임 로직의 로컬 변수
-	static float duration = 6.f;
-	static float elapsedTime = 0.f;
-	static float currentDegree = 0.f;
-	static float rotateSpeed = 180.f;
-	static float distance = 250.f;
+    // 게임 로직의 로컬 변수
+    static float duration = 6.f;
+    static float elapsedTime = 0.f;
+    static float currentDegree = 0.f;
+    static float rotateSpeed = 180.f;
+    static float distance = 250.f;
+    static std::random_device rd;
+    static std::mt19937 mt(rd());
+    static std::uniform_real_distribution<float> randomY(-200.f, 200.f);
+
+    elapsedTime = Math::Clamp(elapsedTime + InDeltaSeconds, 0.f, duration);
+    if (elapsedTime == duration)
+    {
+        lineStart = Vector2(-400.f, randomY(mt));
+        lineEnd = Vector2(400.f, randomY(mt));
+        elapsedTime = 0.f;
+    }
+
+    // 점의 위치 결정
+    currentDegree = Math::FMod(currentDegree + rotateSpeed * InDeltaSeconds, 360.f);
+    float sin = 0.f, cos = 0.f;
+    Math::GetSinCos(sin, cos, currentDegree);
+    point = Vector2(cos, sin) * distance;
 }
 
 // 렌더링 로직을 담당하는 함수
 void SoftRenderer::Render2D()
 {
-	// 렌더링 로직에서 사용하는 모듈 내 주요 레퍼런스
-	auto& r = GetRenderer();
-	const auto& g = Get2DGameEngine();
+    // 렌더링 로직에서 사용하는 모듈 내 주요 레퍼런스
+    auto& r = GetRenderer();
+    const auto& g = Get2DGameEngine();
 
-	// 렌더링 로직의 로컬 변수
-	static float radius = 5.f;
-	static std::vector<Vector2> circle;
+    // 렌더링 로직의 로컬 변수
+    static float radius = 5.f;
+    static std::vector<Vector2> circle;
 
-	if (circle.empty())
-	{
-		for (float x = -radius; x <= radius; ++x)
-		{
-			for (float y = -radius; y <= radius; ++y)
-			{
-				Vector2 target(x, y);
-				float sizeSquared = target.SizeSquared();
-				float rr = radius * radius;
-				if (sizeSquared < rr)
-				{
-					circle.push_back(target);
-				}
-			}
-		}
-	}
+    if (circle.empty())
+    {
+        for (float x = -radius; x <= radius; ++x)
+        {
+            for (float y = -radius; y <= radius; ++y)
+            {
+                Vector2 target(x, y);
+                float sizeSquared = target.SizeSquared();
+                float rr = radius * radius;
+                if (sizeSquared < rr)
+                {
+                    circle.push_back(target);
+                }
+            }
+        }
+    }
 
-	// 붉은 색으로 점 그리기
-	for (auto const& v : circle)
-	{
-		r.DrawPoint(v + point, LinearColor::Red);
-	}
+    // 붉은 색으로 점 그리기
+    for (auto const& v : circle)
+    {
+        r.DrawPoint(v + point, LinearColor::Red);
+    }
 
-	// 투영할 라인 그리기
-	r.DrawLine(lineStart, lineEnd, LinearColor::Black);
+    // 투영할 라인 그리기
+    r.DrawLine(lineStart, lineEnd, LinearColor::Black);
+
+    // 투영된 위치와 거리 계산
+    Vector2 unitV = (lineEnd - lineStart).GetNormalize();
+    Vector2 u = point - lineStart;
+    Vector2 projV = unitV * (u.Dot(unitV));
+    Vector2 projectedPoint = lineStart + projV;
+    float distance = (projectedPoint - point).Size();
+
+    // 투영된 점 그리기
+    for (auto const& v : circle)
+    {
+        r.DrawPoint(v + projectedPoint, LinearColor::Magenta);
+    }
+
+    // 투영 라인 그리기
+    r.DrawLine(projectedPoint, point, LinearColor::Gray);
+
+    // 관련 데이터 화면 출력
+    r.PushStatisticText(std::string("Point : ") + point.ToString());
+    r.PushStatisticText(std::string("Projected Point : ") + projectedPoint.ToString());
+    r.PushStatisticText(std::string("Distance : ") + std::to_string(distance));
 }
 
 // 메시를 그리는 함수
