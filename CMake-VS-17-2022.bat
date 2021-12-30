1ae51d8b9dad697daf32ecbc493198657f0dd3ee
guru@dustinlee.me <Dustin Lee>
2021-12-30 17:46:34 +0900

    변경사항 반영 불필요한 폴더 제거

---
+++ CMake-VS-17-2022.bat
@@ -0,0 +1,33 @@
+@echo off
+
+@echo:
+@echo [SoftRenderer Project Generator]
+@echo -------------------------------------------------
+PUSHD %~dp0
+@echo Removing previous project folder.
+IF EXIST "Project" rd "Project" /s /q
+@echo:
+@echo Done!
+@echo:
+@echo -------------------------------------------------
+IF NOT EXIST "Project" @echo Creating a new project folder.
+IF NOT EXIST "Project" md Project
+IF NOT EXIST "Project" @echo:
+cd Project
+@echo:
+@echo Done!
+@echo:
+@echo -------------------------------------------------
+@echo Running CMAKE script.
+@echo:
+
+cmake -G "Visual Studio 17 2022" ..\
+
+@echo:
+@echo Done!
+@echo:
+@echo -------------------------------------------------
+@echo A new solution file is generated in %~dp0Project
+@echo:
+POPD
+PAUSE
